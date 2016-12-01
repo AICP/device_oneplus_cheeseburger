1881f921274d0ec385857d4b1e9cab2f0e0dfb20
Maxwen <max.weninger@gmail.com>
2016-12-01 11:14:03 +0000

    Fix a2dp stream issues
    Change-Id: Ie4f14678b38b4883994785bf611221baa0175885

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,11 +24,5 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME "OnePlus 5"
-#define BLUETOOTH_QTI_SW TRUE
-// Disables read remote device feature
-#define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #endif
