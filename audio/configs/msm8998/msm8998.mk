839936d49497f9b10f33af2900476ba4c42c36bd
gtpitch <bmontagu@gmail.com>
2017-07-13 00:55:43 -0400

    cheeseburger: fix streaming bt audio issues
    Change-Id: Ied919b2a82df54f873256e046523505ab91d395c
    Signed-off-by: gtpitch <bmontagu@gmail.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- audio/configs/msm8998/msm8998.mk
+++ audio/configs/msm8998/msm8998.mk
@@ -98,14 +98,8 @@ PRODUCT_COPY_FILES += \
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
-ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    device/oneplus/cheeseburger/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
-else
-PRODUCT_COPY_FILES += \
-    device/oneplus/cheeseburger/audio/configs/msm8998/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
-endif
-PRODUCT_COPY_FILES += \
+    device/oneplus/cheeseburger/audio/configs/msm8998/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
