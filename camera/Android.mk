4df6050d1163cfe6fc12013e947a3d0b8706a539
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-21 15:28:42 +0200

    Build this OSS camera HAL3
    Change-Id: I53620cfff1dd040732ecda19e1b50d79e772e8fd

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017 The halogenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,16 +15,4 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_C_INCLUDES := \
-    frameworks/av/include
-
-LOCAL_SRC_FILES := \
-    CameraParameters.cpp
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_MODULE := libcamera_parameters_ext
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_STATIC_LIBRARY)
+include device/oneplus/oneplus2/camera/hal/Android.mk
