1af1b002c35ca3fc990034d9cffc043c9f60d76c
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-21 16:52:36 +0200

    device.mk: remove product copy files for audio
    Change-Id: I674fdb4d172335f2a9e7731081208b8351f2563c

--- device.mk
+++ device.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2017 The MoKee Open Source Project
 #
@@ -100,6 +101,13 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener \
     tinymix
+
+PRODUCT_COPY_FILES += \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
 -include $(LOCAL_PATH)/audio/configs/msm8998/msm8998.mk
 
