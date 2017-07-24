578add001ebc84c3e48b9a01233a1d9f27b45390
Ralf Luther <luther.ralf@gmail.com>
2017-07-24 23:38:51 +0200

    oneplus5: Add custom power HAL
    This simple power HAL will configure the "sys.perf.profile" property upon
    receiving performance-profile change requests, so that init can parse
    "sys.perf.profile" and make appropriate changes. This allows for greater
    customization and simplicity when configuring performance profiles.
    
    Supported power modes: powersave, balanced, performance.
    
    Also features the tap-to-wake feature.
    
    based on the work of Sultanxda <sultanxda@gmail.com> for the OnePlus3
    
    Change-Id: I3570f0e1bca84d7103ee4469ff260d891ae251a3
    Signed-off-by: Ralf Luther <luther.ralf@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -232,6 +232,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_POWERHAL_VARIANT := none
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
