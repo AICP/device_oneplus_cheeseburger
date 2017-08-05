409b0daac6e9301bb95645af8848568e0e54a130
gtpitch <bmontagu@gmail.com>
2017-08-04 21:55:12 -0400

    cheeseburger: audio: sync codes with OOS OTA 4.5.8
    Change-Id: I6f33bafdc087595f039de58938de711430bb7d5f
    Signed-off-by: gtpitch <bmontagu@gmail.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- audio/configs/msm8998/msm8998.mk
+++ audio/configs/msm8998/msm8998.mk
@@ -102,7 +102,7 @@ PRODUCT_COPY_FILES += \
 
 # Reduce client buffer size for fast audio output tracks
 PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1
+    af.fast_track_multiplier=2
 
 # Low latency audio buffer size in frames
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -110,9 +110,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ##fluencetype can be "fluence" or "fluencepro" or "none"
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.qc.sdk.audio.fluencetype=none\
+ro.qc.sdk.audio.fluencetype=fluencepro\
 persist.audio.fluence.voicecall=true\
-persist.audio.fluence.voicerec=false\
+persist.audio.fluence.voicerec=true\
 persist.audio.fluence.speaker=true
 
 #disable tunnel encoding
@@ -166,7 +166,7 @@ audio.dolby.ds2.hardbypass=true
 
 #Disable Multiple offload sesison
 PRODUCT_PROPERTY_OVERRIDES += \
-audio.offload.multiple.enabled=false
+audio.offload.multiple.enabled=true
 
 #Disable Compress passthrough playback
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -186,7 +186,7 @@ audio.safx.pbe.enabled=true
 
 #parser input buffer size(256kb) in byte stream mode
 PRODUCT_PROPERTY_OVERRIDES += \
-audio.parser.ip.buffer.size=262144
+audio.parser.ip.buffer.size=0
 
 #flac sw decoder 24 bit decode capability
 PRODUCT_PROPERTY_OVERRIDES += \
