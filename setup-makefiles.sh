03dc7f6e41077dd6e2a7a642f2413f292b3a7abe
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-09 21:09:52 +0200

    cheeseburger: add list of proprietary files, update extract scripts
    Change-Id: I6e83d4ee89c4e4771289c1a37b76eec44adcb53a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,13 +22,14 @@ VENDOR=oneplus
 
 INITIAL_COPYRIGHT_YEAR=2017
 
-# Load extractutils and do some sanity checks
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 CM_ROOT="$MY_DIR"/../../..
 
 HELPER="$CM_ROOT"/vendor/aicp/build/tools/extract_utils.sh
+
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
