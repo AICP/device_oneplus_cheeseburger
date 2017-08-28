ecec65914be0880337c9591823236794b0691948
Darragh McGee <darraghmcgee20@gmail.com>
2017-08-28 23:54:46 +0100

    cheeseburger: Add TARGET_CRYPTFS_HW_PATH to BoardConfig
    Change-Id: I1c3038e79c624379e77d3ab0ff0c2d966a0157ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,6 +192,8 @@ BOARD_USES_QCNE := true
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
 
 # Display
 BOARD_USES_ADRENO := true
