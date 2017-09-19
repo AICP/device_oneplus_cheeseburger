3f46ed38e3f6f4b3255b27a68cd77337596b3a74
Martinusbe <martinusbe@gmail.com>
2017-09-19 17:25:01 +0200

    op5: add camera hal
    Change-Id: I5671169682dd90390899951243015c904e136672

--- device.mk
+++ device.mk
@@ -164,6 +164,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
     Snap
 
 # Connectivity Engine support (CNE)
