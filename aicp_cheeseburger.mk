cc4690e7e288f819686585d544350b312048251f
Carlos Solano <csolano@gmail.com>
2018-09-24 10:48:31 +0200

    cheeseburger: AICPfy
    Change-Id: I3462b0333daca8d03b2b00285ca0830046a5df70

--- aicp_cheeseburger.mk
+++ aicp_cheeseburger.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_cheeseburger
+PRODUCT_NAME := aicp_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
