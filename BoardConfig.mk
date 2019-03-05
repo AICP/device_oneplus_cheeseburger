8dbbcee11292eef129daff8384d6a82e33e2c590
Carlos Solano <csolano@gmail.com>
2019-03-05 07:58:44 +0100

    cheeseburger: read board-info file
    Change-Id: I3a0598ac122e7885f5459e7221c2071afa3f37e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@
 DEVICE_PATH := device/oneplus/cheeseburger
 
 # Assertions
+TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := cheeseburger,OnePlus5
 
 # Bluetooth
